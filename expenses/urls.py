55c5e8a0d8247643bc1f12520bfd49d357f900e2
Anonymous <>
2023-11-20 17:42:29 +0200

    cleaned🧹

--- expenses/urls.py
+++ expenses/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from moneyflow.views import frontpage
+from django.urls import path, include
+from moneyflow.views import *
+import moneyflow.urls
+import users.urls
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", frontpage, name="frontpage"),
-]
+    path("", include(moneyflow.urls)),
+    path("users/", include(users.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
